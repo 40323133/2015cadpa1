ae2dbdc25ebb798bc6a53b6862ad648aa02e8b42
40323142 <40323142@gm.nfu.edu.tw>
2016-01-14 23:04:20 +0800

    40323133

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40323133 個人網頁','https://github.com/40323133/2015cadpa1'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
